ba81c6869379f7e202f4e693bf7094542ec9b922
Xourabh7 <sourabhalabanoor@gmail.com>
2024-06-13 15:09:08 +0530

    new changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import { Outlet } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <div>Welcome</div>
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
   )
 }
 
