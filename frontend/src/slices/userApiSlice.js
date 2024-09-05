162194862816c2dce0fda452add7fc0738286419
Alabanoor <sourabh-r.alabanoor@capgemini.com>
2024-09-05 22:22:45 +0530

    Login slice and screen

---
+++ frontend/src/slices/userApiSlice.js
@@ -0,0 +1,17 @@
+import { apiSlice } from "./apiSlice";
+import { USERS_URL } from "../constants";
+
+export const userApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (data) => ({
+        url: USERS_URL / auth,
+        method: "POST",
+        body: data,
+      }),
+      keepUnusedDataFor: 5,
+    }),
+  }),
+});
+
+export const { useLoginMutation } = userApiSlice;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./slices/apiSlice";
 import cartSliceReducers from "./slices/cartSlice";
+import authSliceReducers from "./slices/authSlice";
+
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     cart: cartSliceReducers,
+    auth: authSliceReducers,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
